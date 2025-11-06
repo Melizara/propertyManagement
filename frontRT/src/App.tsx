efc5b28198f8b840688de19f76995d768a48e4f7
Melizara <melizaramelizara@gmail.com>
2025-11-06 06:33:24 +0100

    Coms

--- frontRT/src/App.tsx
+++ frontRT/src/App.tsx
@@ -1,3 +1,4 @@
+//Outlet est une zone ou le contenu des pages enfants s'affichent
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -7,11 +8,16 @@ import Login from "./pages/auth/Login";
 import Story from "./pages/Story";
 import Write from "./pages/forms/Write";
 import { useEffect } from "react";
+//useDispatch permet d'envoyer une action Redux
 import { useDispatch } from "react-redux";
+//AppDispatch sert a typer le dispatch avec typescript
 import type { AppDispatch } from "./apps/Store.tsx";
+//Tsy mila manoratra anle localhost:5000 iny tsony fa efa ato zay
 import axios from "./axios"
+//Action Redux pour recuperer les informations de l'user connectee
 import { account } from "./features/authSlice";
 
+//Layout est un modele de page commun,Outlet change selon la route
 const Layout = () => {
   return (
     <>
@@ -22,13 +28,14 @@ const Layout = () => {
   )
 }
 
+//Creation de toute les routes
 const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
       {
         path: "/",
-        element: <Home/>
+        element: <Home />
       },
       {
         path: "/register",
@@ -47,24 +54,27 @@ const router = createBrowserRouter([
         element: <Write />
       },
       {
-        path:"/update/:id",
-        element:<Write/>
+        path: "/update/:id",
+        element: <Write />
       }
     ]
   }
 ]);
 
 function App() {
-  const dispatch= useDispatch<AppDispatch>();
+  const dispatch = useDispatch<AppDispatch>();
 
-  useEffect(()=>{
+  useEffect(() => {
+    //Le token de l'utilisateur deja connecter est stocker dans le localStorage su navigateur
     const token = localStorage.getItem("token");
 
-    if(token){
-      axios.defaults.headers.common["Authorization"]=`Bearer ${token}`
+    if (token) {
+      //Ajouter automatiquement le token dans le header Authorization
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
+      //Action Redux qui recupere le profil utilisateur deja connecter
       dispatch(account());
     }
-  },[dispatch])
+  }, [dispatch])
   return <RouterProvider router={router} />
 }
 
